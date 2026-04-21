be9057a97e29abcbb1b71c8cbbaf655e7074c430
chaitanya_code24 <chaitanyalokhande2412work@gmail.com>
2026-04-21 23:41:30 +0530

    feat(config): add environment-based configuration system

---
+++ config/settings.py
@@ -0,0 +1,11 @@
+﻿from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    groq_api_key: str
+    model_name: str = "llama-3.3-70b-versatile"
+    base_url: str = "https://api.groq.com/openai/v1"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
